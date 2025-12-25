3581792ba2ae765d6f4b01569b62581d087e9cd3
Aspera <1322625193@qq.com>
2025-12-25 16:31:59 +0800

    small fix 2

--- src/main/java/com/tencent/wxcloudrun/GlobalExceptionHandler.java
+++ src/main/java/com/tencent/wxcloudrun/GlobalExceptionHandler.java
@@ -5,7 +5,9 @@ import java.lang.Void;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.config.BizException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(BizException.class)
     public ApiResponse handleBiz(BizException e) {
